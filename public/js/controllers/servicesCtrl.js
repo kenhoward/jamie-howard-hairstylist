c4386802298fbb8ded07ad74c7c5e7f4af7430a8
Kenny Howard <ken.chad.howard@gmail.com>
2015-03-24 15:05:42 -0600

    instagram api input success

--- public/js/controllers/servicesCtrl.js
+++ public/js/controllers/servicesCtrl.js
@@ -1,5 +1,28 @@
 var app = angular.module('noServer');
 
-app.controller('servicesCtrl', function($scope) {
-	
+app.controller('servicesCtrl', function($scope, $http) {
+	$scope.pictures = [];
+
+	$scope.getPictures = 	(function() { //creating a truthy
+		$http({
+			method: 'GET',
+			url: '/instagramFeed'
+		}).then(function(res) {
+			console.log(res.data.data);
+			var data = res.data.data;
+			for (var i = 0; i < data.length; i++) {
+				var instaObj = {};
+				instaObj.lowRes = data[i].images.low_resolution.url //
+				instaObj.comments = data[i].comments.count // comments count
+				instaObj.likes = data[i].likes.count // likes count
+				instaObj.urlLink = data[i].link // link to instagram post
+				instaObj.timestamp = data[i].created_time // time stamp
+				console.log(instaObj)
+				$scope.pictures.push(instaObj);
+
+			}
+
+		})
+	})()
+
 })
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 
 var app = express();
 
@@ -7,6 +8,13 @@ var port = 8117;
 // MIDDLEWARE ===================================
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/instagramFeed', function(req, res) {
+	request.get('https://api.instagram.com/v1/users/1075315762/media/recent/?client_id=e1ce9a8f9ae2445b933dceed9bd3850e&min_id=873291822536584574_1075315762', function(err, response, body) {
+		console.log("response", response);
+		res.send(body);
+	})
+})
 
 // CONNECTION(S) ================================
 
