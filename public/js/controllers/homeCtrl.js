d4962daf525873b13c8384b9db5d8053c885692f
Kenny Howard <ken.chad.howard@gmail.com>
2015-03-30 13:39:35 -0600

    styling home view

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -2,7 +2,7 @@ var app = angular.module('noServer');
 
 app.controller('homeCtrl', function($scope) {
 	$scope.homeInterval = 3000;
-	var slides = $scope.slides = ['images/carousel1.jpg', 'images/carousel2.jpg', 'images/carousel3.jpeg'];
+	var slides = $scope.slides = ['images/homeCaro1.jpg', 'images/homeCaro2.jpg', 'images/homeCaro3.jpg'];
 	// $scope.addSlide = function() {
 	// 	var newWidth = 600 + slides.length + 1;
 	// 	slides.push ({
