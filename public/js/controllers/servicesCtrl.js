f86f69c3e270df830228b4c34084b2f66289d2d9
Kenny Howard <ken.chad.howard@gmail.com>
2015-03-30 17:24:52 -0600

    adding comments and likes to pictures

--- public/js/controllers/servicesCtrl.js
+++ public/js/controllers/servicesCtrl.js
@@ -14,11 +14,11 @@ app.controller('servicesCtrl', function($scope, $http) {
 			var data = res.data.data;
 			for (var i = 0; i < data.length; i++) {
 				var instaObj = {};
-				instaObj.lowRes = data[i].images.low_resolution.url //
-				instaObj.comments = data[i].comments.count // comments count
-				instaObj.likes = data[i].likes.count // likes count
+				instaObj.lowRes = data[i].images.low_resolution.url
+				instaObj.comments = data[i].comments.count // <i class="fa fa-comment"></i>
+				instaObj.likes = data[i].likes.count // FA: Hallow Heart <i class="fa fa-heart-o"></i> // Solid: <i class="fa fa-heart"></i>
 				instaObj.urlLink = data[i].link // link to instagram post
-				instaObj.timestamp = data[i].created_time // time stamp
+				instaObj.timestamp = data[i].created_time // <i class="fa fa-clock-o"></i>
 				console.log(instaObj)
 				$scope.pictures.push(instaObj);
 
